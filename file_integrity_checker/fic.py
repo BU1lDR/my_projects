2967645306ee85ff38f2fd87b5d3216bcac76a9f
Aryan Verma <aryanverma102007@gmail.com>
2026-08-26 16:56:48 +0530

    The scanner now works recursively

--- file_integrity_checker/fic.py
+++ file_integrity_checker/fic.py
@@ -13,7 +13,7 @@ def calculate_hash(file_path):
 
 folder = Path("test_data")
 
-for i in folder.iterdir():
+for i in folder.rglob("*"):
     if i.is_file():
         file_hash = calculate_hash(i)
 
