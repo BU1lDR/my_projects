e44c22a615357025dbd7653534c841a5af64d06f
Aryan Verma <aryanverma102007@gmail.com>
2026-08-26 17:30:28 +0530

    included a file counter to count the no. of scanned files

--- file_integrity_checker/fic.py
+++ file_integrity_checker/fic.py
@@ -13,10 +13,16 @@ def calculate_hash(file_path):
 
 folder = Path("test_data")
 
+file_count = 0
 for i in folder.rglob("*"):
     if i.is_file():
+
+        file_count += 1 # A file counter
+
         file_hash = calculate_hash(i)
 
         print(f"[FILE] {i}")
         print(f"       SHA-256: {file_hash}")
         print()
+
+print(f"{file_count} files scanned")
